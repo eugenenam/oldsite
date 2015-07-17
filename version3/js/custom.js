33bf14c4aaa44374c3a07c61473560ef485d389a
Eugene Nam <yoogeen@gmail.com>
2015-07-17 18:07:14 -0400

    add version 3

---
+++ version3/js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    /** 
+     * This part does the "fixed navigation after scroll" functionality
+     * We use the jQuery function scroll() to recalculate our variables as the 
+     * page is scrolled/
+     */
+    $(window).scroll(function(){
+        var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
+        var div_top = $('#nav-anchor').offset().top;
+            if (window_top > div_top) {
+                $('nav').addClass('stick');
+            } else {
+                $('nav').removeClass('stick');
+            }
+    });
+
